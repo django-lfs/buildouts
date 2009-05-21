82f9be6cd390bdafa6718ee0d3a3da956bde0af1
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-05-21 17:12:09 +0000

    Issue 19: Added lfs and lfstheme to INSTALLED_APPS; removed dirs from TEMPLATE_DIRS.
    git-svn-id: http://django-lfs.googlecode.com/svn/buildouts@343 70813176-cce2-11dd-87b6-9d1a99559f42

--- development/lfs_project/settings.py
+++ development/lfs_project/settings.py
@@ -12,9 +12,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''      # Or path to database file if using sqlite3.
-DATABASE_USER = ''         # Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'lfs_portlets'      # Or path to database file if using sqlite3.
+DATABASE_USER = 'root'         # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
@@ -78,8 +78,6 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(DIRNAME, "..", "parts", "svn_lfs", "lfstheme", "templates"),
-    os.path.join(DIRNAME, "..", "parts", "svn_lfs", "lfs", "templates"),
 )
 
 INSTALLED_APPS = (
@@ -93,6 +91,9 @@ INSTALLED_APPS = (
     "pagination",
     'reviews',
     "tagging",
+    "portlets",
+    "lfstheme",
+    "lfs",
     "lfs.tests",
     'lfs.core',
     'lfs.caching',
@@ -107,11 +108,13 @@ INSTALLED_APPS = (
     'lfs.order',
     'lfs.page',    
     'lfs.payment',
+    'lfs.portlets',    
     'lfs.search',
     'lfs.shipping',
     'lfs.tagging',
     'lfs.tax',
     'lfs.utils',
+    "south",
 )
 
 FORCE_SCRIPT_NAME=""
