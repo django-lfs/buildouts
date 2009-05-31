b8dd8caf87bd797d39218cb24ae66a73b319a818
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-05-31 06:49:15 +0000

    Cleaned up urls.
    git-svn-id: http://django-lfs.googlecode.com/svn/buildouts@391 70813176-cce2-11dd-87b6-9d1a99559f42

--- quickstart/lfs_project/urls.py
+++ quickstart/lfs_project/urls.py
@@ -8,6 +8,11 @@ import os
 DIRNAME = os.path.dirname(__file__)
 
 urlpatterns = patterns("",
+    (r'', include('lfs.core.urls')),
+    (r'^manage/', include('lfs.manage.urls')),
+)
+
+urlpatterns += patterns("",
     (r'^contact/', include('contact_form.urls')),
 )
 
@@ -18,9 +23,4 @@ urlpatterns += patterns("",
 urlpatterns += patterns("",    
     (r'^admin/(.*)', admin.site.root),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(DIRNAME, "media"), 'show_indexes': True }),
-)
-
-urlpatterns += patterns("",
-    (r'', include('lfs.core.urls')),
-    (r'^manage/', include('lfs.manage.urls')),
 )
