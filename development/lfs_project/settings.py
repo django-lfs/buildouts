b5f5d085725f644bedae64431c5c44fb3abfe652
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-06-11 19:09:53 +0000

    Added LFS_PAYPAL_REDIRECT to settings.py
    git-svn-id: http://django-lfs.googlecode.com/svn/buildouts@419 70813176-cce2-11dd-87b6-9d1a99559f42

--- development/lfs_project/settings.py
+++ development/lfs_project/settings.py
@@ -12,9 +12,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'lfs_trunk'      # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'         # Not used with sqlite3.
+DATABASE_ENGINE = ''      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = ''      # Or path to database file if using sqlite3.
+DATABASE_USER = ''         # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
@@ -155,3 +155,4 @@ EMAIL_HOST_PASSWORD = ""
 
 PAYPAL_RECEIVER_EMAIL = "info@yourbusiness.com"
 PAYPAL_IDENTITY_TOKEN = "set_this_to_your_paypal_pdt_identity_token"
+LFS_PAYPAL_REDIRECT = True
