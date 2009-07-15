6513e0f83c4eedca41df94ad42a0e9d13091c58b
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-07-15 09:42:10 +0000

    git-svn-id: http://django-lfs.googlecode.com/svn/buildouts@683 70813176-cce2-11dd-87b6-9d1a99559f42

--- development/lfs_project/settings.py
+++ development/lfs_project/settings.py
@@ -7,15 +7,15 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+    ('Kai Diefenbach', 'kai.diefenbach@iqpp.de'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''      # Or path to database file if using sqlite3.
-DATABASE_USER = ''         # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_ENGINE = 'mysql'      # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'lfs_migration'      # Or path to database file if using sqlite3.
+DATABASE_USER = 'kai'         # Not used with sqlite3.
+DATABASE_PASSWORD = 'kd176u9x'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
@@ -24,11 +24,11 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Berlin'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'de'
 
 SITE_ID = 1
 
@@ -43,7 +43,7 @@ MEDIA_ROOT = DIRNAME + "/media"
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://localhost:8000/media/'
+MEDIA_URL = '/media/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
@@ -88,6 +88,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     "django.contrib.flatpages",
     "django.contrib.sitemaps",
+    "demmelhuber_theme",
     "lfstheme",
     "pagination",
     'reviews',
@@ -115,8 +116,8 @@ INSTALLED_APPS = (
     'lfs.tax',
     'lfs.utils',
     'paypal.standard.ipn',
-    'paypal.standard.pdt',    
-    "south",
+    'paypal.standard.pdt',
+     "south",
 )
 
 FORCE_SCRIPT_NAME=""
@@ -139,20 +140,27 @@ AUTHENTICATION_BACKENDS = (
 # For sql_queries
 INTERNAL_IPS = (
     "127.0.0.1",
+    "87.106.213.234",
 )
 
 # CACHE_BACKEND = 'file:///'
 # CACHE_BACKEND = 'locmem:///'
 # CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
-CACHE_BACKEND = 'dummy:///'
+# CACHE_BACKEND = 'dummy:///'
+CACHE_BACKEND = 'memcached://127.0.0.1:11216/?timeout=0'
 
-CATEGORY_PREFIX = "category-"
 LFS_RECENT_PRODUCTS_LIMIT = 5
 
-EMAIL_HOST = ""
-EMAIL_HOST_USER = ""
-EMAIL_HOST_PASSWORD = ""
+DEFAULT_FROM_EMAIL = "info@demmelhuber.net"
+EMAIL_HOST = "smtp.1und1.com"
+EMAIL_HOST_USER = "pt6979730-usne"
+EMAIL_HOST_PASSWORD = "sJHUsjsJJ234"
 
-PAYPAL_RECEIVER_EMAIL = "info@yourbusiness.com"
+PAYPAL_RECEIVER_EMAIL = "paypal@demmelhuber.net"
 PAYPAL_IDENTITY_TOKEN = "set_this_to_your_paypal_pdt_identity_token"
-LFS_PAYPAL_REDIRECT = True
+LFS_PAYPAL_REDIRECT = False
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
